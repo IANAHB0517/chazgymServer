9d0d36a7c31f4c4e26a1314a322e4633f245f2cc
IANAHB0517 <fromtheedge0517@gmail.com>
2024-06-09 00:17:56 +0900

    project set typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import * as common from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -8,9 +8,38 @@ import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
 import { PriceModule } from './price/price.module';
 import { PaymentModule } from './payment/payment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import {
+  ENV_DB_DATABASE_KEY,
+  ENV_DB_HOST_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USERNAME_KEY,
+} from './common/const/env-keys.const';
 
-@Module({
-  imports: [AuthModule, CommonModule, LessonModule, OrderModule, UserModule, PriceModule, PaymentModule],
+@common.Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env[ENV_DB_HOST_KEY],
+      port: parseInt(process.env[ENV_DB_PORT_KEY]),
+      username: process.env[ENV_DB_USERNAME_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY],
+    }),
+    AuthModule,
+    CommonModule,
+    LessonModule,
+    OrderModule,
+    UserModule,
+    PriceModule,
+    PaymentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
