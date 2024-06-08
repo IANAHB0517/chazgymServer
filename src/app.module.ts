4b298db5d7980567feed6c9ec83e432977438efd
IANAHB0517 <fromtheedge0517@gmail.com>
2024-06-08 23:36:33 +0900

    project reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { LessonModule } from './lesson/lesson.module';
+import { OrderModule } from './order/order.module';
+import { UserModule } from './user/user.module';
+import { PriceModule } from './price/price.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CommonModule, LessonModule, OrderModule, UserModule, PriceModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
